ad02664207ed051952bbe36e1912a236c949c2da
Yao Yuan <yeaya@163.com>
2025-12-29 18:41:45 +0800

    add  dependency info in lib include file, etc.

--- main/share/include/java.desktop.h
+++ main/share/include/java.desktop.h
@@ -1,5 +1,9 @@
 #ifndef _java$desktop_
 #define _java$desktop_
+//$ dependency java.prefs:17.35
+//$ dependency java.datatransfer:17.35
+//$ dependency java.xml:17.35
+//$ dependency java.base:17.35
 
 #include <java/lang/def.h>
 
--- test/share/classes/java.desktop.test.h
+++ test/share/classes/java.desktop.test.h
@@ -1,5 +1,12 @@
 #ifndef _java$desktop$test_
 #define _java$desktop$test_
+//$ dependency java.desktop:17.35
+//$ dependency java.logging:17.35
+//$ dependency jdk.httpserver:17.35
+//$ dependency java.prefs:17.35
+//$ dependency java.datatransfer:17.35
+//$ dependency java.xml:17.35
+//$ dependency java.base:17.35
 
 #include <java/lang/def.h>
 
